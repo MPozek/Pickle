ef9daccd46a7df60eda7d36c6f9777a838f1dc58
MPozek <mislav@exordiumgames.com>
2021-09-14 10:23:39 +0200

    refactor - move auto pick mode and extensions to a separate cs file

---
+++ Runtime/AutoPickMode.cs
@@ -0,0 +1,27 @@
+﻿namespace Pickle
+{
+    public enum AutoPickMode
+    {
+        None, GetComponent, GetComponentInChildren, FindObject, GetComponentInParent
+    }
+
+    public static class AutoPickExtensions
+    {
+        public static UnityEngine.Object DoAutoPick(this AutoPickMode mode, UnityEngine.Object fromObject, System.Type targetType)
+        {
+            switch (mode)
+            {
+                case AutoPickMode.GetComponent:
+                    return ((Component)fromObject).GetComponent(targetType);
+                case AutoPickMode.GetComponentInChildren:
+                    return ((Component)fromObject).GetComponentInChildren(targetType);
+                case AutoPickMode.GetComponentInParent:
+                    return ((Component)fromObject).GetComponentInParent(targetType);
+                case AutoPickMode.FindObject:
+                    return GameObject.FindObjectOfType(targetType);
+            }
+
+            throw new System.NotImplementedException($"Auto picking for mode {mode} is not implemented!");
+        }
+    }
+}
--- Runtime/PickleAttribute.cs
+++ Runtime/PickleAttribute.cs
@@ -36,29 +36,4 @@ namespace Pickle
     {
         Window, Dropdown
     }
-
-    public enum AutoPickMode
-    {
-        None, GetComponent, GetComponentInChildren, FindObject, GetComponentInParent
-    }
-
-    public static class EnumExtensions
-    {
-        public static UnityEngine.Object DoAutoPick(this AutoPickMode mode, UnityEngine.Object fromObject, System.Type targetType)
-        {
-            switch (mode)
-            {
-                case AutoPickMode.GetComponent:
-                    return ((Component)fromObject).GetComponent(targetType);
-                case AutoPickMode.GetComponentInChildren:
-                    return ((Component)fromObject).GetComponentInChildren(targetType);
-                case AutoPickMode.GetComponentInParent:
-                    return ((Component)fromObject).GetComponentInParent(targetType);
-                case AutoPickMode.FindObject:
-                    return GameObject.FindObjectOfType(targetType);
-            }
-
-            throw new System.NotImplementedException($"Auto picking for mode {mode} is not implemented!");
-        }
-    }
 }
