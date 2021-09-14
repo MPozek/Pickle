873835dfb14eaad6132e6914b0dbb26bb6a50e52
MPozek <mislav@exordiumgames.com>
2021-09-14 10:22:26 +0200

    add constructors to pickle attribute
    enums are well named so constructors should actually remove the bloat of using named parameters

--- Runtime/PickleAttribute.cs
+++ Runtime/PickleAttribute.cs
@@ -11,6 +11,25 @@ namespace Pickle
         public PickerType PickerType = PickerType.Dropdown;
         public string FilterMethodName = null;
         public AutoPickMode AutoPickMode = AutoPickMode.None;
+
+        public PickleAttribute(ObjectProviderType providerType, string filterMethod = null)
+        {
+            LookupType = providerType;
+            FilterMethodName = filterMethod;
+        }
+
+        public PickleAttribute(ObjectProviderType providerType, AutoPickMode autoPick, string filterMethod = null)
+        {
+            LookupType = providerType;
+            AutoPickMode = autoPick;
+            FilterMethodName = filterMethod;
+        }
+
+        public PickleAttribute(AutoPickMode autoPick, string filterMethod = null)
+        {
+            AutoPickMode = autoPick;
+            FilterMethodName = filterMethod;
+        }
     }
 
     public enum PickerType
